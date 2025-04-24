ce1eb92942583abcbad4797fc9682a7201efac97
Osman Mehmood <osman.mehmood@PC-1F-05.appedology.internal>
2025-04-23 23:21:44 -0700

    remove comments

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def login_screen():
-    st.header("Please log in.")
+    st.header("Private App within your Organization.")
     st.button("Log in with Microsoft", on_click=st.login)
 
 if not st.experimental_user.is_logged_in:
@@ -10,7 +10,3 @@ else:
     st.header(f"Welcome, {st.experimental_user.name}!")
     st.button("Log out", on_click=st.logout)
 
-
-# Add role assigning functionality with database for storing roles assigning(only by admin): admin or user
-# User listing (Password reset by admin)
-# Each user will have access to view specific dashboard. So the dashboard/analysis on left menu will only be visible if user has access to view specific analysis.    
